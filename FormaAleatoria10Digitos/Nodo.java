6a5009d179891d7abcc335e9e95c8d74f1efa1e1
Frank0823SC <71744464+Frank0823SC@users.noreply.github.com>
2020-12-02 13:37:19 -0600

    Create Nodo.java

---
+++ FormaAleatoria10Digitos/Nodo.java
@@ -0,0 +1,11 @@
+package FormaAleatoria10Digitos;
+public class Nodo {
+    
+    int dato;
+    Nodo sig;
+
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.sig = null;
+    }
+}
